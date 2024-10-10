8356ce220cb12c55ebb071c3154890a0f9b1a000
CedricCHAM <cedric.chambaudry@gmail.com>
2024-10-10 23:33:44 +0200

    ajout des commentaire

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-from keras import *
-import numpy as np
+from keras import * # Importe toutes les fonctions et classes du module keras
+import numpy as np # Importe la bibliothèque NumPy pour le calcul numérique
 
-model = Sequential()
+model = Sequential() # Initialise un modèle séquentiel Keras
 
-model.add(Input(shape=(1,)))
-model.add(layers.Dense(units=3))
-model.add(layers.Dense(units=64))
-model.add(layers.Dense(units=1))
-
-
-entree = np.array([1, 2, 3, 4, 5])
-sortie = np.array([6, 7, 8, 9, 10])
+model.add(Input(shape=(1,))) # Ajoute une couche d'entrée avec une dimension d'entrée de 1
+model.add(layers.Dense(units=3)) # Ajoute une couche dense avec 3 unités
+model.add(layers.Dense(units=64)) # Ajoute une autre couche dense avec 64 unités
+model.add(layers.Dense(units=1)) # Ajoute une dernière couche dense avec 1 unité
 
 
-model.compile(loss='mean_squared_error', optimizer='adam')
+entree = np.array([1, 2, 3, 4, 5]) # Crée un tableau NumPy pour les entrées du modèle
+sortie = np.array([6, 7, 8, 9, 10]) # Crée un tableau NumPy pour les sorties correspondantes
 
-model.fit(x=entree, y=sortie, epochs=500)
 
-while True:
-    x = int(input('Nombre :'))
-    x_np = np.array([[x]])  # Redimensionner pour correspondre à l'entrée attendue
-    print('Prediction :' + str(model.predict(x_np)))
+model.compile(loss='mean_squared_error', optimizer='adam') # Compile le modèle avec la perte d'erreur quadratique moyenne et l'optimiseur Adam
+model.fit(x=entree, y=sortie, epochs=500) # Entraîne le modèle sur les données d'entrée et de sortie pendant 500 époques
+
+while True: # Boucle infinie pour prédire les valeurs
+    x = int(input('Nombre :')) # Prend un nombre en entrée de l'utilisateur
+    x_np = np.array([[x]])  # Convertit ce nombre en tableau NumPy 2D
+    print('Prediction :' + str(model.predict(x_np))) # Prédit et affiche la sortie du modèle pour cette entrée
 
 
           
